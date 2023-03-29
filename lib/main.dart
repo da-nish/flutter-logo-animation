598ba3f3fc66eb2291a3ea3f0cb4611cde2fbf52
Danish Khan <danishdev64@gmail.com>
2023-03-30 01:28:06 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:rotate_animation/rotate1.dart';
+import 'package:rotate_animation/rotate2.dart';
+import 'package:rotate_animation/rotate3.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // LogoRotate(),
+            Rotate1(),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class LogoRotate extends StatefulWidget {
+  const LogoRotate({super.key});
+
+  @override
+  State<LogoRotate> createState() => LogoRotateState();
+}
+
+class LogoRotateState extends State<LogoRotate> {
+  double turns = 0.0;
+
+  void _changeRotation() {
+    setState(() => turns += 1.0 / 8.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: _changeRotation,
+          child: const Text('Rotate Logo'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(50),
+          child: AnimatedRotation(
+            turns: turns,
+            duration: const Duration(milliseconds: 200),
+            child: const FlutterLogo(),
+          ),
+        ),
+      ],
+    );
+  }
+}
